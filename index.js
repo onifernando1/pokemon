b8bdc5649f457af1106991d83b2c83413281d437
onifernando1 <onifernando1@gmail.com>
2023-06-19 00:39:09 +0100

    crop player image

--- index.js
+++ index.js
@@ -22,10 +22,16 @@ image.onload = () => {
   const scaledWidth = image.width * 4; // Calculate the scaled width
   const scaledHeight = image.height * 4; // Calculate the scaled height
   c.imageSmoothingEnabled = false; // Disable image smoothing
-  c.drawImage(image, -1100, -700, scaledWidth, scaledHeight); // Start at house
+  c.drawImage(image, -930, -850, scaledWidth, scaledHeight); // Start at house
   c.drawImage(
     playerImage,
+    0, // x start crop
+    0, // y start crop
+    playerImage.width / 4, //crop one section of image (x axis),
+    playerImage.height, // crop one section of image (y axis)
     canvas.width / 2 - playerImage.width / 2,
-    canvas.height / 2 - playerImage.height / 2
+    canvas.height / 2 - playerImage.height / 2,
+    playerImage.width / 4, //size to render
+    playerImage.height // size to render
   ); // Declare player image after map loads as map larger, place in center
 };
