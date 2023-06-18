2f121d385c7216c428d493a5162fcc1c45556966
onifernando1 <onifernando1@gmail.com>
2023-06-19 00:13:46 +0100

    import image on load

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ console.log("hello world");
 const canvas = document.querySelector("canvas");
 canvas.width = 1024;
 canvas.height = 576;
+
 const c = canvas.getContext("2d");
+
 c.fillStyle = "white";
 c.fillRect(0, 0, canvas.width, canvas.height);
+
+const image = new Image(); // create HTML Image
+image.src = "./assets/images/town.png";
+
+//Only call drawImage after image loaded
+image.onload = () => {
+  c.drawImage(image, 0, 0); //arg = html img, x position, y position)
+};
