1a3e0bebe7b23e03a37984c1f9c101db0e200722
onifernando1 <onifernando1@gmail.com>
2023-06-19 00:33:50 +0100

    center image

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ image.src = "./assets/images/town.png";
 image.style.transform = "scale(4)";
 image.style.imageRendering = "pixelated";
 
+const playerImage = new Image();
+playerImage.src = "./assets/images/playerDown.png";
+playerImage.onload = () => {};
+
 //Only call drawImage after image loaded
 image.onload = () => {
   const scaledWidth = image.width * 4; // Calculate the scaled width
   const scaledHeight = image.height * 4; // Calculate the scaled height
   c.imageSmoothingEnabled = false; // Disable image smoothing
   c.drawImage(image, -1100, -700, scaledWidth, scaledHeight); // Start at house
+  c.drawImage(
+    playerImage,
+    canvas.width / 2 - playerImage.width / 2,
+    canvas.height / 2 - playerImage.height / 2
+  ); // Declare player image after map loads as map larger, place in center
 };
