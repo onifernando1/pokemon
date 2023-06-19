f7d72b75a458fb167448bf0120c05a49c2441520
onifernando1 <onifernando1@gmail.com>
2023-06-19 21:11:49 +0100

    maths to calculate if player overlapping with battlezone

--- index.js
+++ index.js
@@ -159,11 +159,24 @@ function animate() {
     for (let i = 0; i < battleZones.length; i++) {
       //loop through every boundary
       const battleZone = battleZones[i];
+      const overlappingArea =
+        (Math.min(
+          player.position.x + player.width,
+          battleZone.position.x + battleZone.width
+        ) -
+          Math.max(player.position.x, battleZone.position.x)) *
+        (Math.min(
+          player.position.y + player.height,
+          battleZone.position.y + battleZone.height
+        ) -
+          Math.max(player.position.y, battleZone.position.y));
+
       if (
         rectangularCollision({
           rectangle1: player,
           rectangle2: battleZone,
-        })
+        }) &&
+        overlappingArea > (player.width * player.height) / 2 //divide by two to increase chance of battle and prevent trigger if travelling along edges
       ) {
         console.log("BATTLE");
         break; // break out as soon as collision, otherwise collision will be false with other boundaries, so not working
